1fa75510e01257b7530d527ab3a9f097d6ece35d
ryanmonro <ryan@ryanmonro.info>
2020-06-29 15:08:38 +1000

    Spinning based on time elapsed since noteOn

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-var MAX = 8;
+var STEPS = 8;
+var ROWS = 8;
+var SPINTIME = 0.6;
+var WIDTH = 480;
+var HEIGHT = 480;
+
 var step = 0;
 
 var canvas = document.querySelector('#zdog');
-canvas.addEventListener('click', function(e){
-  var col = Math.floor(e.offsetX * 8 / 480);
-  var row = Math.floor(e.offsetY * 8 / 480);
-  grid[row][col].on = !grid[row][col].on;
-})
 
-// create illo
 let illo = new Zdog.Illustration({
-  // set canvas with selector
   element: '#zdog',
   dragRotate: true,
-  // zoom: 3
+  zoom: 1
 });
 
-// add circles
-
 var grid = [];
-for (var row = 0; row < MAX; row++){
+for (var row = 0; row < ROWS; row++){
   var gridRow = [];
-  for (var i = 0; i < MAX; i++){
-    var stroke = 10;
-    var green = '3'; // vs '8' for playing maybe
+  for (var i = 0; i < STEPS; i++){
     var ellipse = new Zdog.Ellipse({
       addTo: illo,
       translate: {
@@ -31,7 +25,7 @@ for (var row = 0; row < MAX; row++){
         y: -240 + 30 + (row * 60),
         z: 0
       },
-      diameter: 20,
+      diameter: 40,
     });
     ellipse.on = Math.random() < 0.5;
     gridRow.push(ellipse);
@@ -40,19 +34,23 @@ for (var row = 0; row < MAX; row++){
 }
 
 function animate() {
-  // rotate illo each frame
-  // illo.rotate.z += 0.03;
-  // illo.rotate.y += 0.03;
   // illo.rotate.x += 0.03;
-  for (var row = 0; row < MAX; row++){
-    for (var i = 0; i < MAX; i++){
+  for (var row = 0; row < ROWS; row++){
+    for (var i = 0; i < STEPS; i++){
       var ellipse = grid[row][i];
-      var green = '7';
+      var green = 'A';
       ellipse.color = '#' + (row * 2).toString(16) + green + (i * 2).toString(16);
-      var timeSinceOn = Zdog.modulo(window.step - i, 8);
-      ellipse.rotate.y = (Zdog.TAU/4)/(timeSinceOn + 1);
-      
+      var time = Tone.Transport.getSecondsAtTime();
+      var timeSinceOn = time - ellipse.noteTime;
+      if (timeSinceOn > 0 
+        && timeSinceOn < SPINTIME
+        ) {
+        ellipse.rotate.y = (Zdog.TAU/4) * Zdog.easeInOut((timeSinceOn / SPINTIME), 2);
+      } else {
+        ellipse.rotate.y = Zdog.TAU/4;
+      }
       ellipse.stroke = ellipse.on ? 10 : 2;
+      ellipse.fill = ellipse.on;
     }
   }
 
@@ -61,12 +59,32 @@ function animate() {
   requestAnimationFrame( animate );
 }
 // start animation
-animate();
 
 var seq = new Tone.Sequence(function(time, note){
   Tone.Draw.schedule(function(){
-    window.step = note;
+    if (window.step != note) {
+      window.step = note;
+      for(row of grid){
+        row[note].noteTime = time;
+      }
+    }
   }, time);
-}, [0,1,2,3,4,5,6,7], "8n").start(0);
+}, range(0,7), "8n").start(0);
 
+canvas.addEventListener('click', function(e){
+  var col = Math.floor(e.offsetX * 8 / 480);
+  var row = Math.floor(e.offsetY * 8 / 480);
+  grid[row][col].on = !grid[row][col].on;
+})
+
+function range(min, max){
+  var arr = [];
+  for(var i = min; i <= max; i++){
+    arr.push(i);
+  }
+  return arr;
+}
+
+
+animate();
 Tone.Transport.start();
