1bb4773cd48aaf495837f7b1ae809f3b4c1086ee
ryanmonro <ryan@ryanmonro.info>
2020-06-29 16:05:22 +1000

    Sounds; determining colours/sizes/clickables from vars

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ var HEIGHT = 480;
 
 var step = 0;
 
+var instruments = ["Kick", "Snare", "Rim", "Clap", "Clav", "Clap", "Mcng", "Lcng"].map(function(name){
+    return {name: name, mute: false, player: new Tone.Player("sounds/" + name + ".WAV").toMaster()};
+});
+for(instrument of instruments){
+  instrument.player.volume.value = -6;
+}
+
 var canvas = document.querySelector('#zdog');
 
 let illo = new Zdog.Illustration({
   element: '#zdog',
-  dragRotate: true,
+  // dragRotate: true,
   zoom: 1
 });
 
@@ -18,34 +25,47 @@ var grid = [];
 for (var row = 0; row < ROWS; row++){
   var gridRow = [];
   for (var i = 0; i < STEPS; i++){
+    var cellWidth = WIDTH / STEPS;
+    var cellHeight = HEIGHT / ROWS;
     var ellipse = new Zdog.Ellipse({
       addTo: illo,
       translate: {
-        x: -240 + 30 + (i * 60),
-        y: -240 + 30 + (row * 60),
+        x: WIDTH / -2 + cellWidth / 2 + (i * cellWidth),
+        y: HEIGHT / -2 + cellHeight / 2 + (row * cellHeight),
         z: 0
       },
-      diameter: 40,
+      width: cellWidth * 2 / 3,
+      height: cellHeight * 2/ 3,
     });
-    ellipse.on = Math.random() < 0.5;
+    var green = 'BB';
+    // ellipse.color = '#' + (row * 2).toString(16) + green + (i * 2).toString(16);
+    ellipse.color = '#' + (Math.floor(0x88 + row * 0x77 / ROWS)).toString(16) + green + (Math.floor(0x33 + i * 0xCC / STEPS)).toString(16);
+    console.log(ellipse.color);
+    ellipse.on = false;// Math.random() < 0.5;
     gridRow.push(ellipse);
   }
   grid.push(gridRow);
 }
+// initalise a beat
+grid[0][0].on = true;
+grid[0][3].on = true;
+grid[0][4].on = true;
+grid[4][0].on = true;
+grid[4][5].on = true;
+grid[7][6].on = true;
+grid[7][7].on = true;
 
 function animate() {
-  // illo.rotate.x += 0.03;
   for (var row = 0; row < ROWS; row++){
     for (var i = 0; i < STEPS; i++){
       var ellipse = grid[row][i];
-      var green = 'A';
-      ellipse.color = '#' + (row * 2).toString(16) + green + (i * 2).toString(16);
       var time = Tone.Transport.getSecondsAtTime();
       var timeSinceOn = time - ellipse.noteTime;
-      if (timeSinceOn > 0 
+      if (timeSinceOn >= 0 
         && timeSinceOn < SPINTIME
         ) {
         ellipse.rotate.y = (Zdog.TAU/4) * Zdog.easeInOut((timeSinceOn / SPINTIME), 2);
+        // ellipse.rotate.y = 0;
       } else {
         ellipse.rotate.y = Zdog.TAU/4;
       }
@@ -53,7 +73,6 @@ function animate() {
       ellipse.fill = ellipse.on;
     }
   }
-
   illo.updateRenderGraph();
   // animate next frame
   requestAnimationFrame( animate );
@@ -61,19 +80,20 @@ function animate() {
 // start animation
 
 var seq = new Tone.Sequence(function(time, note){
-  Tone.Draw.schedule(function(){
-    if (window.step != note) {
-      window.step = note;
-      for(row of grid){
-        row[note].noteTime = time;
-      }
+  for(row in grid){
+    if (grid[row][note].on == true) {
+      instruments[row].player.start(time);
     }
-  }, time);
-}, range(0,7), "8n").start(0);
+  }
+  window.step = note;
+  for(row of grid){
+    row[note].noteTime = time;
+  }
+}, range(0, STEPS - 1), "8n").start(0);
 
 canvas.addEventListener('click', function(e){
-  var col = Math.floor(e.offsetX * 8 / 480);
-  var row = Math.floor(e.offsetY * 8 / 480);
+  var col = Math.floor(e.offsetX * STEPS / WIDTH);
+  var row = Math.floor(e.offsetY * ROWS / HEIGHT);
   grid[row][col].on = !grid[row][col].on;
 })
 
@@ -84,7 +104,6 @@ function range(min, max){
   }
   return arr;
 }
-
 
 animate();
 Tone.Transport.start();
